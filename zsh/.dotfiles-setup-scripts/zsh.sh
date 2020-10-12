b3ba0fc92d6bd65f14feb629baa44e66b1bb60ea
X1aomu <xiaomublue@gmail.com>
2020-10-12 23:40:31 +0800

    modify: zsh: enable zsh-autosuggestions

--- zsh/.dotfiles-setup-scripts/zsh.sh
+++ zsh/.dotfiles-setup-scripts/zsh.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if ! `which fzf > /dev/null`; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    ~/.fzf/install
+fi
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
