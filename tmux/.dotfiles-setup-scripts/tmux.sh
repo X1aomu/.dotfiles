e167aac1f5df59c32e208df5ec0fad95fd5a5c3f
X1aomu <xiaomublue@gmail.com>
2020-10-12 23:16:43 +0800

    new(setup-script): vim, zsh and tmux

---
+++ tmux/.dotfiles-setup-scripts/tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+tmux source ~/.tmux.conf
---
+++ vim/.dotfiles-setup-scripts/vim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vim +PlugUpgrade +PlugInstall +PlugUpdate +qall
